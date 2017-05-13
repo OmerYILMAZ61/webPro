f1923dd9e3543313eefbdc0cb6aa04af03bc851c
vektorel <vektorel@lab8-4>
2017-05-13 16:48:19 +0300

    das

---
+++ Java-19-JSF-Project-master/ProjeTakipUygulamasi/src/com/vektorel/projetakip/bean/ProjeBean.java
@@ -0,0 +1,99 @@
+package com.vektorel.projetakip.bean;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.vektorel.projetakip.dao.DAO;
+import com.vektorel.projetakip.model.Durum;
+import com.vektorel.projetakip.model.Proje;
+
+@ManagedBean
+@SessionScoped
+public class ProjeBean {
+
+	private Proje projeAdd;
+	private Durum selectedDurum;
+	String selectedDurumString;
+	private List<Durum> durumList ;
+	private List<Proje> projeList;
+	private boolean excelControl = false;
+	
+	@PostConstruct
+	public void init(){
+		this.projeAdd = new Proje();
+		this.projeList = DAO.getInstance().getProjeList();
+		this.durumList = DAO.getInstance().getDurumList();
+	}
+	
+	
+	
+	public boolean isExcelControl() {
+		return excelControl;
+	}
+
+
+
+	public void setExcelControl(boolean excelControl) {
+		this.excelControl = excelControl;
+	}
+
+	public boolean degistiriciFonk(){
+	
+		this.excelControl = !this.excelControl;
+		
+		return excelControl;
+	}
+
+	public String addProjeFonk(){
+		projeAdd.setDurum(selectedDurum);
+		DAO.getInstance().addProje(projeAdd);
+		this.projeList = DAO.getInstance().getProjeList();
+		this.projeAdd = new Proje();
+		
+		return "listele.xhtml";
+	}
+	
+	public void setProjeAdd(Proje projeAdd) {
+		this.projeAdd = projeAdd;
+	}
+	
+	public Proje getProjeAdd() {
+		return projeAdd;
+	}
+	
+	public void setDurumList(List<Durum> durumList) {
+		this.durumList = durumList;
+	}
+	
+	public List<Durum> getDurumList() {
+		return durumList;
+	}
+	
+	public void setSelectedDurum(Durum selectedDurum) {
+		this.selectedDurum = selectedDurum;
+	}
+	
+	public Durum getSelectedDurum() {
+		return selectedDurum;
+	}
+	
+	public void setSelectedDurumString(String selectedDurumString) {
+		this.selectedDurumString = selectedDurumString;
+	}
+	
+	public String getSelectedDurumString() {
+		return selectedDurumString;
+	}
+	
+	public void setProjeList(List<Proje> projeList) {
+		this.projeList = projeList;
+	}
+	
+	public List<Proje> getProjeList() {
+		return projeList;
+	}
+	
+}
